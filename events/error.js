abe00b2404c0e3e40a8702706e601ba5fef00a5e
Ugur <uguralgul23@gmail.com>
2022-10-01 11:14:03 +0200

    Added Error Channel

---
+++ events/error.js
@@ -0,0 +1,13 @@
+const channel = client.channels.cache.get('1025687301751312445')
+const client = require('..');
+
+
+client.on('message',message => {
+  // Ignore bots
+  if (message.author.bot) return
+  // Send the embed
+  const embed = new Discord.MessageEmbed()
+    .setDescription(message.content)
+    .setAuthor(message.author.tag, message.author.displayAvatarURL())
+  channel.send(embed).catch(console.error)
+})
