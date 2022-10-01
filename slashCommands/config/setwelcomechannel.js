3dd6e44dea19c76659cb3845e19b4f352a58c988
Ugur <uguralgul23@gmail.com>
2022-10-01 11:25:52 +0200

    Added Welcome Channel

---
+++ slashCommands/config/setwelcomechannel.js
@@ -0,0 +1,31 @@
+const Schema = require('../../models/welcomeChannel')
+
+module.exports = {
+  name: 'setchannel',
+  description: 'Sets the welcome channel',
+  options: [
+    {
+        name: "channel",
+        description: "The channel",
+        type: 8,
+        required: true
+    }
+  ],
+  run: async (client, interaction) => {
+    const channel = interaction.options.getChannel('channel')
+    if (!channel) return interaction.reply({ content: 'Please specify a channel!'})
+
+    Schema.findOne({ Guild: interaction.guild.id}, async (err, data) => {
+      if (data) {
+        data.Channel = channel.id
+        data.save()
+      } else {
+        new Schema ({ 
+          Guild: interaction.guild.id,
+          Channel: channel.id
+        }).save()
+      }
+      interaction.reply({ content: `${channel} has been set as your welcome channel`})
+    })
+  }
+}
